364a0fd220238a49dd15d7bf33189059e8f9eb29
sreehari <sreehari.prasad@suprsend.com>
2025-12-03 11:53:09 +0530

    Refactor translation list command for improved clarity and structure
    - Moved workspace and management client initialization to the beginning of the command execution for better readability.
    - Enhanced the loading message to provide feedback on the number of listed translation locales.
    - Maintained existing error handling for fetching translations while ensuring consistent command flow.

--- internal/commands/category/category_translation/translation_list.go
+++ internal/commands/category/category_translation/translation_list.go
@@ -15,6 +15,10 @@ var translationListCmd = &cobra.Command{
 	Short: "List preference translations",
 	Long:  "List preference translations",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		workspace, _ := cmd.Flags().GetString("workspace")
+		mgmntClient := utils.GetSuprSendMgmntClient()
+
 		var p *pin.Pin
 		if !utils.IsOutputPiped() {
 			p = pin.New("Loading...",
@@ -24,17 +28,18 @@ var translationListCmd = &cobra.Command{
 			cancel := p.Start(context.Background())
 			defer cancel()
 		}
-		workspace, _ := cmd.Flags().GetString("workspace")
-		mgmntClient := utils.GetSuprSendMgmntClient()
+
 		translations, err := mgmntClient.ListPreferenceTranslations(workspace)
 		if err != nil {
 			log.WithError(err).Error("Couldn't fetch translations")
 			return
 		}
+
 		msg := fmt.Sprintf("Listed %d translation locales from %s", len(translations.Results), workspace)
 		if p != nil {
 			p.Stop(msg)
 		}
+
 		outputType, _ := cmd.Flags().GetString("output")
 		if len(translations.Results) == 0 && utils.IsOutputPiped() {
 			utils.OutputData([]interface{}{}, outputType)
